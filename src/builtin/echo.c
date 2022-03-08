2e3543ddaa92b3c22d2708e6e53864b3088ca6d0
aabajyan <arsen.abajyan@pm.me>
2022-03-08 15:28:28 +0400

    Fix: catch flags like on bash.

--- src/builtin/echo.c
+++ src/builtin/echo.c
@@ -6,29 +6,51 @@
 /*   By: aabajyan <aabajyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/26 17:56:56 by araramya          #+#    #+#             */
-/*   Updated: 2022/03/07 21:10:22 by aabajyan         ###   ########.fr       */
+/*   Updated: 2022/03/08 15:26:50 by aabajyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+static bool	echo_check_flag(bool *endline, char *arg)
+{
+	size_t	i;
+
+	i = 0;
+	if (arg && arg[i] == '-')
+	{
+		while (arg[++i] != '\0')
+			if (arg[i] != 'n')
+				return (false);
+		if (*endline == true)
+			*endline = false;
+		return (true);
+	}
+	return (false);
+}
+
 int	builtin_echo(int argc, char **argv)
 {
 	bool	endline;
+	bool	check_needed;
 	int		i;
 
 	i = 1;
 	endline = true;
+	check_needed = true;
 	if (argc > 1)
 	{
-		endline = (ft_strcmp("-n", argv[1]));
-		if (!endline)
-			i++;
 		while (i < argc)
 		{
-			printf("%s", argv[i++]);
-			if (i < argc)
-				printf(" ");
+			if (!check_needed || !echo_check_flag(&endline, argv[i]))
+			{
+				if (check_needed)
+					check_needed = false;
+				printf("%s", argv[i]);
+				if (i + 1 < argc)
+					printf(" ");
+			}
+			i++;
 		}
 	}
 	if (endline)
